695d7bfdb3d5b01276f4d422e37fddf7835392fe
Michael Bernhart <michael.bernhart@oberalp.com>
2022-03-13 17:56:00 +0100

    Initial Commit

---
+++ LOTR Converter/LOTR_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTR_ConverterApp.swift
+//  LOTR Converter
+//
+//  Created by OakHost Customer on 13/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct LOTR_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
